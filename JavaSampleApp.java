6afa8974232cae96a9728739fd2b43d0a4feb499
sylvestersmile <sylvrubby@gmail.com>
2024-10-17 17:00:20 +0100

    Create JavaSampleApp.java

---
+++ JavaSampleApp.java
@@ -0,0 +1,12 @@
+// src/main/java/com/example/JavaSampleApp.java
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSampleApp {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSampleApp.class, args);
+    }
+}
